be5a0085b2f202ba4fb4ddc0b763777816b8a618
MarijnSt <marijn.stammeleer@gmail.com>
2019-06-27 13:58:37 +0200

    only display when feedback has been given

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,17 +8,21 @@ const Button = (props) => {
 }
 
 const Display = (props) => {
-    return (
-        <div className='results'>
-            <h1>Statistics</h1>
-            <p>Good: { props.good }</p>
-            <p>Neutral: { props.neutral }</p>
-            <p>Bad: { props.bad }</p>
-            <p>Total: { props.total }</p>
-            <p>Average: { props.average }</p>
-            <p>Positive: { props.positive } %</p>
-        </div>
-    )
+    if(props.total === 0) {
+        return <p>No feedback given</p>
+    } else {
+        return (
+            <div className='results'>
+                <h1>Statistics</h1>
+                <p>Good: { props.good }</p>
+                <p>Neutral: { props.neutral }</p>
+                <p>Bad: { props.bad }</p>
+                <p>Total: { props.total }</p>
+                <p>Average: { props.average }</p>
+                <p>Positive: { props.positive } %</p>
+            </div>
+        )
+    }
 }
 
 const App = (props) => {
